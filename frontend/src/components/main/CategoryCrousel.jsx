79b8caf62f665e9adb64d933d7fb316b91842f2d
Asif-hash786 <masif15881@gmail.com>
2026-08-20 13:45:41 +0530

    fixed layout

--- frontend/src/components/main/CategoryCrousel.jsx
+++ frontend/src/components/main/CategoryCrousel.jsx
@@ -17,10 +17,9 @@ const CategoryCrousel = () => {
         <CarouselContent>
           {
             category.map((cat, index) => (
-                <CarouselItem className="md:basis-1/2 lg-basis-1/3">
+                <CarouselItem className="md:basis-1/2 lg-basis-1/3" key={index}>
                   <Button variant='outline' className="rounded-xl">{cat}</Button>
                 </CarouselItem>
-              
             ))
           }
         </CarouselContent>
--- frontend/src/pages/Jobs.jsx
+++ frontend/src/pages/Jobs.jsx
@@ -9,15 +9,15 @@ const Jobs = () => {
   return (
     <div>
       <Navbar />
-      <div className='max-w-7xl mx-auto mt-5'>
-        <div className='w-20%'>
+      <div className='max-w-7xl mx-auto mt-5 flex'>
+        <div className='w-[50%]'>
           <FilterCard />
         </div>
         <div className='flex gap-5'>
           {
             jobsArray.length <= 0 ? <div>jobs not found</div> : (
               <div className='flex-1 h-[88vh] overflow-y-auto pb-5'>
-                <div className='grid grid-cols-3 gap-4'>
+                <div className='grid grid-cols-1 lg:grid-cols-3  gap-4'>
                   {
                     jobsArray.map((item,index)=>{
                       return(
